80f5d23ce29229f6ec2906d0bd4fd1c511973b60
Jouni K. Seppänen <jks@iki.fi>
2019-03-23 09:02:13 +0200

    Test coverage report

--- setup.py
+++ setup.py
@@ -6,39 +6,38 @@ from setuptools import setup
 with open("README.md") as f:
     readme = f.read()
 
-requirements = [
-    'parsimonious~=0.8.1',
-    'toolz~=0.9.0',
-]
+requirements = ["parsimonious~=0.8.1", "toolz~=0.9.0"]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ["pytest-runner"]
 
-test_requirements = ['pytest', ]
+test_requirements = ["pytest", "pytest-cov"]
+dev_requirements = test_requirements
 
 setup(
     author="Jouni K. Seppänen",
-    author_email='jks@iki.fi',
+    author_email="jks@iki.fi",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Environment :: Web Environment',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Text Processing :: Markup :: HTML',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Web Environment",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Text Processing :: Markup :: HTML",
     ],
     description="htexpr compiles an html string into a Python expression",
     install_requires=requirements,
     license="MIT license",
     long_description=readme,
     include_package_data=True,
-    name='htexpr',
-    packages=['htexpr'],
+    name="htexpr",
+    packages=["htexpr"],
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/jkseppan/htexpr',
-    version='0.0.1',
+    extras_require={"dev": dev_requirements},
+    url="https://github.com/jkseppan/htexpr",
+    version="0.0.1",
     zip_safe=True,
 )
