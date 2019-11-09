373a946d7c7a375ced5a4f04fdd3394df3ef3d3a
Jouni K. Seppänen <jks@iki.fi>
2019-11-09 11:17:02 +0200

    Allow python 3.8
    and run circleci tests on that

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         "Environment :: Web Environment",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Text Processing :: Markup :: HTML",
     ],
